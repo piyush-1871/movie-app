cf10315174f1b6db06475753af6a232e03943a69
piyush-1871 <piyush1871b@gmail.com>
2023-01-04 23:16:35 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import rootReducer from "./reducers/index";
-import { createStore } from "redux";
+import combineReducers from "./reducers/index";
+import { legacy_createStore, applyMiddleware } from "redux";
 
-const store = createStore(rootReducer);
+//  function logger(obj, next, action)
+// logger(obj)(next)(action)
+const logger = function({ dispatch, getState }){
+  return function(next){
+    return function (action){
+      // middleware code
+      console.log('ACTION_TYPE = ', action.type);
+      next(action);
+    }
+  }
+}
+const store = legacy_createStore(combineReducers, applyMiddleware(logger));
 // console.log('store',store);
 // console.log('beforeState',store.getState());
 
