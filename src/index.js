f7cae9a912115c6f3148f3ab2d9632317e090353
piyush-1871 <piyush1871b@gmail.com>
2023-01-04 14:53:13 +0530

    dispatching actions

--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ import { createStore } from "redux";
 
 const store = createStore(movies);
 console.log('store',store);
-console.log('state',store.getState());
+console.log('beforeState',store.getState());
+
+store.dispatch({
+  type:'ADD_MOVIES',
+  movies:[{name: 'Superman'}]
+})
+console.log('afterState',store.getState());
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
