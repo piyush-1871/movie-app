644369070a51211adfd533a95c2af7ebe74f987b
piyush-1871 <piyush1871b@gmail.com>
2023-01-04 14:47:04 +0530

    creating the store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import movies from "./reducers/index";
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(movies);
+console.log('store',store);
+console.log('state',store.getState());
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
