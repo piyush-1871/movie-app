e533e71346466dc62507294232f9d8b1bbb4baa9
piyush-1871 <piyush1871b@gmail.com>
2023-01-04 15:08:45 +0530

    subscribe to the store

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ import movies from "./reducers/index";
 import { createStore } from "redux";
 
 const store = createStore(movies);
-console.log('store',store);
-console.log('beforeState',store.getState());
+// console.log('store',store);
+// console.log('beforeState',store.getState());
 
-store.dispatch({
-  type:'ADD_MOVIES',
-  movies:[{name: 'Superman'}]
-})
-console.log('afterState',store.getState());
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies:[{name: 'Superman'}]
+// })
+// console.log('afterState',store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>
 );
