8d08235d70e10fbc38fb4a2b066e3a4e112f8e9b
piyush-1871 <piyush1871b@gmail.com>
2023-01-04 23:24:21 +0530

    modifying middleware

--- src/index.js
+++ src/index.js
@@ -7,15 +7,26 @@ import { legacy_createStore, applyMiddleware } from "redux";
 
 //  function logger(obj, next, action)
 // logger(obj)(next)(action)
-const logger = function({ dispatch, getState }){
-  return function(next){
-    return function (action){
+/*
+const logger = function ({ dispatch, getState }) {
+  return function (next) {
+    return function (action) {
       // middleware code
-      console.log('ACTION_TYPE = ', action.type);
+      console.log("ACTION_TYPE = ", action.type);
       next(action);
-    }
-  }
-}
+    };
+  };
+};
+*/
+
+const logger =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    // logger code
+    console.log("ACTION_TYPE = ", action.type);
+    next(action);
+  };
 const store = legacy_createStore(combineReducers, applyMiddleware(logger));
 // console.log('store',store);
 // console.log('beforeState',store.getState());
